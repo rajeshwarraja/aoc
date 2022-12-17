cdbe0a754acd6036987ad4c6b7fc74f2475e6822
Rajeshwar Raja <rajeshwarraja@gmail.com>
2022-12-18 01:38:02 +0530

    Solved day 13

--- src/2022/13.js
+++ src/2022/13.js
@@ -24,33 +24,28 @@ const sampleInput = `[1,1,3,1,1]
 [1,[2,[3,[4,[5,6,7]]]],8,9]
 [1,[2,[3,[4,[5,6,0]]]],8,9]`
 
-function get(value) {
+function convertToArray(value) {
     let result = []
     let level = 0
     let index = -1
     let stack = [result]
     while(++index < value.length) {
-	// console.log(`Value: ${value[index]} @ ${index}`)
 	if(value[index] === '[') {
 	    if(++level > 1) {
 		let values = []
 		stack[stack.length - 1].push(values)
 		stack.push(values)
 	    }
-	    // console.log(`  > ${level}`)	    
 	} else if(value[index] === ']') {
 	    if(level-- > 1) {
 		stack.pop()
 	    }
-	    
-	    // console.log(`  < ${level}`)
 	} else if(value[index] === ',') {
 	    continue
 	} else {
 	    const end1 = value.indexOf(',', index)
 	    const end2 = value.indexOf(']', index)
 	    let end = end1 >= 0 && end2 >= 0? Math.min(end1, end2) : (end1 >= 0? end1 : end2)
-
 	    stack[stack.length - 1].push(Number(value.slice(index, end)))
 	    index = end - 1
 	}
@@ -60,8 +55,6 @@ function get(value) {
 }
 
 function isCorrectOrder(left, right) {
-    // console.log(`Compare ${JSON.stringify(left)} vs ${JSON.stringify(right)}`)
-    
     let index = -1
     const length = Math.max(left.length, right.length)
     while(++index < length) {
@@ -78,7 +71,6 @@ function isCorrectOrder(left, right) {
 	    if(result === undefined) continue
 	    return result
 	} else {
-	    // console.log(`  Compare ${JSON.stringify(nextLeft)} vs ${JSON.stringify(nextRight)}`)
 	    if(nextLeft < nextRight) return true
 	    if(nextLeft > nextRight) return false
 	}
@@ -88,26 +80,37 @@ function isCorrectOrder(left, right) {
 
 function solvePart1(input) {
     return input.split('\n\n').reduce((sum, pair, index) => {
-	const [left, right] = pair.split('\n').map(get)
+	const [left, right] = pair.split('\n').map(convertToArray)
 	if(isCorrectOrder(left, right)) {
-	    // console.log('Right order')
 	    sum = sum + (index + 1)
-	} else {
-	    // console.log('Not in right order')
 	}
-	// console.log()
 	return sum
     }, 0)
 }
 
 function solvePart2(input) {
+    let packets = input.split('\n').filter((row) => row.length > 0).map(convertToArray)
 
+    packets.push(convertToArray('[[2]]'), convertToArray('[[6]]'))
+
+    return  packets.sort((left, right) => {
+	const result = isCorrectOrder(left, right)
+	if(result === undefined) return 0
+	if(result) return -1
+	return 1
+    }).reduce((key, packet, index) => {
+	const packetString = JSON.stringify(packet)
+	if(packetString === '[[2]]' || packetString === '[[6]]')
+	    return key = key * (index + 1)
+
+	return key
+    }, 1)
 }
 
 function run() {
     console.log('********* Day 13 *********')
     console.log(`Part 1: ${solvePart1(puzzleInput)}`)
-    console.log(`Part 2: ${solvePart2(sampleInput)}`)
+    console.log(`Part 2: ${solvePart2(puzzleInput)}`)
 }
 
 module.exports = {
