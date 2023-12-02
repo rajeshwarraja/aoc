d55080b1f9cc9457a3ee24f25e0f1fe708b99119
Rajeshwar Raja <rajeshwarraja@gmail.com>
2023-12-02 07:17:18 +0530

    day 2

---
+++ src/2023/2.js
@@ -0,0 +1,23 @@
+const puzzleInput = require('./input.js').day2
+
+const sampleInput1 = `
+`
+
+const sampleInput2 = `
+`
+
+function solvePart1(input) {
+}
+
+function solvePart2(input) {
+}
+
+function run() {
+    console.log('********* Day 2 **********')
+    console.log(`Part 1: ${solvePart1(puzzleInput)}`)
+    console.log(`Part 2: ${solvePart2(puzzleInput)}`)
+}
+
+module.exports = {
+    run: run
+}
